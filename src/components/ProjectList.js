c17049902c40c15bb1d9305766019062d1d45250
aarongurovich <aarongur@outlook.com>
2024-08-10 12:20:34 -0700

    added new project

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,6 +5,13 @@ import ProjectCard from './ProjectCard';
 
 const projects = [
   {
+    title: 'Birthday Reminder',
+    description: "Shows users how close friends' birthday is.",
+    link: 'https://birthday-reminder-4nh.pages.dev/',
+    image: './assets/images/birthday.PNG',
+  },
+
+  {
     title: 'Tours Website',
     description: 'Can browse through several tour options.',
     link: 'https://tour-website.pages.dev/',
@@ -13,7 +20,7 @@ const projects = [
   {
     title: 'Reviews Component',
     description: 'Can browse through several reviews from different people.',
-    link: 'https://c5fd88fa.reviews-bvb.pages.dev/',
+    link: 'https://reviews-bvb.pages.dev/',
     image: './assets/images/reviews.PNG',
   },
 
