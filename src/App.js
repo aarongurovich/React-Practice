82b126c976fa9bfd43bea124e0d75fbf24ca5f5e
aarongurovich <aarongur@outlook.com>
2024-08-10 16:35:03 -0700

    changed name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My React Practice Projects</h1>
+        <h1>My Reactfolio</h1>
       </header>
       <main>
         <ProjectList />
