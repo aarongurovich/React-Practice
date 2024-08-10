5843d078ef679c92d08c0c08ab474e7b45cf0cfe
aarongurovich <aarongur@outlook.com>
2024-08-09 22:46:35 -0700

    fixed file paths

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -8,21 +8,21 @@ const projects = [
     title: 'Tours Website',
     description: 'Can browse through several tour options. Extra practice using useEffect and useState',
     link: 'https://tour-website.pages.dev/',
-    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/tours.PNG'),
+    image: './assets/images/tours.PNG',
   },
   {
     title: 'Random Movie Generator',
     description: 'Can Generate Random movie based on certain filters such as genre, ratings, date published, etc..',
     link: 'https://movie-generator-app.pages.dev/',
-    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/randommovie.PNG'),
+    image: './assets/images/randommovie.PNG',
   },
   {
     title: 'Reddit Clone',
     description: 'Can search for posts and explore different topics across various communities.',
     link: 'https://reddit-client-5io.pages.dev/',
-    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/redditclone.PNG'),
+    image: './assets/images/redditclone.PNG',
   },
- 
+  
  
 ];
 
