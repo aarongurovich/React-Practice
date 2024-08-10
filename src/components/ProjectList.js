9bf4923e152f66a6522a6e4f21f5d42b0749a41c
aarongurovich <aarongur@outlook.com>
2024-08-09 21:36:33 -0700

    first

---
+++ src/components/ProjectList.js
@@ -0,0 +1,44 @@
+
+import React from 'react';
+import ProjectCard from './ProjectCard';
+
+
+const projects = [
+  {
+    title: 'Tours Website',
+    description: 'Can browse through several tour options. Extra practice using useEffect and useState',
+    link: 'https://tour-website.pages.dev/',
+    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/tours.png'),
+  },
+  {
+    title: 'Random Movie Generator',
+    description: 'Can Generate Random movie based on certain filters such as genre, ratings, date published, etc..',
+    link: 'https://movie-generator-app.pages.dev/',
+    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/randommovie.PNG'),
+  },
+  {
+    title: 'Reddit Clone',
+    description: 'Can search for posts and explore different topics across various communities.',
+    link: 'https://reddit-client-5io.pages.dev/',
+    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/redditclone.PNG'),
+  },
+  {
+    title: 'Portfolio Website',
+    description: 'Can search for posts and explore different topics across various communities.',
+    link: 'https://reddit-client-5io.pages.dev/',
+    image: require('C:/Users/Aaron/react-portfolio/src/assets/images/redditclone.PNG'),
+  },
+ 
+];
+
+const ProjectList = () => {
+  return (
+    <div className="project-list">
+      {projects.map((project, index) => (
+        <ProjectCard key={index} {...project} />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectList;
